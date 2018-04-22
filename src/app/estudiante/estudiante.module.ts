6a67b36db1630c5b4715532d284af6a506af9afe
Clodo <claudiobidau@gmail.com>
2018-04-22 16:19:04 -0300

    Listado de estudiantes

--- src/app/estudiante/estudiante.module.ts
+++ src/app/estudiante/estudiante.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EstudianteListaComponent } from './lista/lista.component';
 import { EstudianteAltaComponent } from './alta/alta.component';
+import { EstudianteService } from './estudiante.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [EstudianteListaComponent, EstudianteAltaComponent]
+  declarations: [
+    EstudianteListaComponent,
+    EstudianteAltaComponent
+  ],
+  providers: [
+    EstudianteService
+  ]
 })
 export class EstudianteModule { }
